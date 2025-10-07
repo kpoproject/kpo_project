4cd5072c3b280fb68da1cbb616161556f6426dbe
Vladislav Isaychev <gregnov69@gmail.com>
2025-10-07 21:26:21 +0300

    modified database setup; modified setup script

--- src/backend/db/set_connections.sql
+++ src/backend/db/set_connections.sql
@@ -1,3 +1,3 @@
-ALTER TABLE users ADD CONSTRAINT fk_collection_id FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE;
+ALTER TABLE collections ADD CONSTRAINT fk_id FOREIGN KEY (id) REFERENCES users (collection_id) ON DELETE CASCADE;
 
 ALTER TABLE collection_entry ADD CONSTRAINT fk_collection_id FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE;
