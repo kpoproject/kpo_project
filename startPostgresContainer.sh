cc619654815bbce452f1bb0487a6a0882cced9ba
BrainRuiner <constantin.m.petroo@gmail.com>
2025-11-24 23:52:10 +0300

    backend starts with docker compose

--- startPostgresContainer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-containerName="some-postgres"
-
-docker run --name ${containerName} -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-docker exec ${containerName} psql -U postgres -f src/backend/sql/create_db.sql
-docker exec ${containerName} psql -U postgres -f src/backend/sql/create_tables.sql
