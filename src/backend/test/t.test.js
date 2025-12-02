5fc553b3b487cf269469cada6b1f87756ef5a05d
Vladislav Isaychev <gregnov69@gmail.com>
2025-12-02 22:04:23 +0300

    ye

--- src/backend/test/t.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, jest, test, describe } from "@jest/globals";
-function isEqualStrict(a, b) {
-  return a === b;
-}
-
-test("equality", () => {
-  expect(isEqualStrict(1, 1)).toBe(true);
-});
