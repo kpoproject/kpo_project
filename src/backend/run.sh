c15bbee26d18c40881d00b54a5013a450a57a045
BrainRuiner <constantin.m.petroo@gmail.com>
2025-12-11 22:50:13 +0300

    made it run

--- src/backend/run.sh
+++ src/backend/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 sudo docker build -t back . &&
-sudo docker run --network=kpo_net -e POSTGRES_PASSWORD=docker -e POSTGRES_USER=postgres -e POSTGRES_DB=libralib -p 1337:8080 back
+  sudo docker run --name kpo_back \
+    --network=kpo_net \
+    -e POSTGRES_PASSWORD=${KPO_POSTGRES_PASSWORD} \
+    -e POSTGRES_DB=${KPO_POSTGRES_DB} \
+    -e POSTGRES_USER=${KPO_POSTGRES_USER} \
+    -e POSTGRES_HOST=${KPO_POSTGRES_HOST} \
+    -e POSTGRES_PORT=${KPO_POSTGRES_PORT} \
+    -d -p 1337:8080 back
--- src/db/run.sh
+++ src/db/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 sudo docker build -t pdb . &&
-  sudo docker run --name pdb_name --network kpo_net -e POSTGRES_PASSWORD=docker -p 5432:5432 pdb
+  sudo docker run \
+  --name pdb_name \
+  --network kpo_net \
+  -e POSTGRES_PASSWORD=${KPO_POSTGRES_PASSWORD} \
+  -d -p 5432:5432 pdb
   #sudo docker run --net kpo_net -p 5432:5432 pdb
